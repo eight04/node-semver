c40da0bfe7d479ff32f10652d6521e08dd705f3d
eight <eight04@gmail.com>
2017-09-06 14:45:34 +0800

    Use rollup-plugin-re

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import uglify from 'rollup-plugin-uglify';
 import re from 'rollup-plugin-re';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
 	input: 'bundle.js',
@@ -10,11 +11,20 @@ export default {
 	name: 'semverCompare',
 	plugins: [re({
 		patterns: [{
+			test: /(\w+) = exports\.(\w+)( =.*)/g,
+			replace: '$1$3\nexports.$2 = $1;'
+		}, {
+			test: /exports\.(\w+\()/g,
+			replace: '$1'
+		}, {
+			test: /exports = module\.exports.*/g,
+			replace: ''
+		}, {
 			test: /debug\(.+?\);/g,
 			replace: ''
 		}, {
-			test: /^\s*\/\* nomin \*\/.+/gm,
+			test: /^\s*(\/\* nomin \*\/).+/gm,
 			replace: ''
 		}]
-	}), uglify()]
+	}), commonjs(), uglify()]
 };
